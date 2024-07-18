5e026a9c15eec483bff69e805cdb144b1810c99c
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2024-07-18 22:22:47 +0200

    create refresh token flow on frontend

--- 04-refresh-jwt-auth/public/authenticated.js
+++ 04-refresh-jwt-auth/public/authenticated.js
@@ -1,3 +1,46 @@
+const REFRESH_TOKEN = "refreshToken"
+const ACCESS_TOKEN = "accessToken"
+
+let timer = null
+
+const setupRefresh = () => {
+  console.log("Setting up interval")
+  timer = setInterval(() => refreshTokenPair(), 4 * 1000)
+}
+
+const saveTokenPair = (accessToken, refreshToken) => {
+  console.log("Saving token pair", { accessToken, refreshToken })
+
+  localStorage.setItem(ACCESS_TOKEN, accessToken)
+  localStorage.setItem(REFRESH_TOKEN, refreshToken)
+}
+
+const refreshTokenPair = async () => {
+  console.log("Refreshing Token")
+
+  const currentRefreshToken = localStorage.getItem(REFRESH_TOKEN)
+
+  const headers = new Headers()
+  headers.append("Authorization", `Bearer ${currentRefreshToken}`)
+
+  const response = await fetch("/api/refresh-tokens", {
+    method: "POST",
+    headers
+  })
+
+  if (!response.ok) {
+    console.error("Could not refresh tokens", response.status)
+    if (response.status === 401 || response.status === 403) {
+      localStorage.clear()
+      setLoggedOut()
+    }
+  }
+
+  const { accessToken, refreshToken } = await response.json()
+
+  saveTokenPair(accessToken, refreshToken)
+}
+
 const login = async () => {
   const username = document.getElementById('username-input').value
   const password = document.getElementById('password-input').value
@@ -18,18 +61,18 @@ const login = async () => {
     return
   }
 
-  const { token } = await response.json();
+  const { accessToken, refreshToken } = await response.json();
 
-  console.log({ token })
+  // think carefully where you store these tokens
+  saveTokenPair(accessToken, refreshToken)
 
-  // think carefully where you store this token
-  localStorage.setItem("token", token)
+  setupRefresh()
 
   setLoggedIn()
 }
 
 const fetchAuthenticatedData = async () => {
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem(ACCESS_TOKEN)
 
   if (token) {
     const headers = new Headers()
@@ -40,12 +83,17 @@ const fetchAuthenticatedData = async () => {
     })
 
     if (!response.ok) {
-      console.error("Something went wrong fetching the secret data")
+      console.error("Something went wrong fetching the secret data", response.status)
+      if (response.status === 401 || response.status === 403) {
+        localStorage.clear()
+        return setLoggedOut()
+      }
     }
 
     const content = await response.json()
 
-    document.getElementById('active-area').innerHTML = `<pre>${JSON.stringify(content, null, 2)}</pre>`
+    document.getElementById('active-area').innerHTML = `<pre>${JSON.stringify(content, null, 2)}</pre>
+    <button onclick="fetchAuthenticatedData()">Fetch authenticated Data</button>`
   }
 }
 
@@ -53,6 +101,8 @@ const setLoggedIn = () => {
   const parent = document.getElementById('active-area')
   parent.innerHTML = `<p>Waiting for secret data</p>
     <button onclick="fetchAuthenticatedData()">Fetch authenticated Data</button>`
+
+  setupRefresh()
 }
 
 const setLoggedOut = () => {
@@ -62,10 +112,22 @@ const setLoggedOut = () => {
     <h4>Password</h4>
     <input id="password-input" type="password" />
     <button onclick="login()">Login</button>`
+  clearInterval(timer)
 }
 
-if (localStorage.getItem("token")) {
+if (localStorage.getItem(ACCESS_TOKEN) && localStorage.getItem(REFRESH_TOKEN)) {
   setLoggedIn()
 } else {
   setLoggedOut()
+}
+
+
+// The spice section
+let savedRefreshToken
+const saveCurrentRefreshToken = () => {
+  savedRefreshToken = localStorage.getItem(REFRESH_TOKEN)
+}
+
+const injectSavedRefreshToken = () => {
+  localStorage.setItem(REFRESH_TOKEN, savedRefreshToken)
 }

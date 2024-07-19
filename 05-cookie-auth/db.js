ce821837c0befee2e10137af3616a8bdf9bf07d6
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2024-07-19 11:12:46 +0200

    create base cookie authentication

---
+++ 05-cookie-auth/db.js
@@ -0,0 +1,31 @@
+const nedb = require('nedb-promise')
+
+const db = new nedb()
+
+const expirationTime = 5 * 1000
+
+const createToken = async (username) => {
+  try {
+    const newToken = await db.insert({ exp: Date.now() + (expirationTime), username })
+
+    return newToken
+  } catch (err) {
+    console.error(`Could not create new token: ${username}, ${err.message}`)
+    throw err
+  }
+}
+
+const getToken = async (tokenId) => {
+  try {
+    const token = await db.findOne({ _id: tokenId })
+
+    return token
+  } catch (err) {
+    console.log(`Could not find token: ${tokenId}, ${err.message}`)
+  }
+}
+
+module.exports = {
+  createToken,
+  getToken
+}
---
+++ 05-cookie-auth/index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const cookieparser = require('cookie-parser')
+require('dotenv').config()
+
+const { createToken } = require('./db')
+const { authenticateCookie } = require('./cookie')
+
+const COOKIE_SECRET = process.env.COOKIE_SECRET
+const COOKIE_KEY = process.env.COOKIE_KEY
+
+const cookieOptions = {
+  maxAge: 60 * 1000,
+  signed: true
+}
+
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(cookieparser(COOKIE_SECRET))
+
+app.get("/", (_, res) => {
+  res.redirect('index.html')
+})
+
+app.post("/api/login", async (req, res) => {
+  console.log({ url: req.url, body: req.body })
+  const { username, password } = req.body
+
+  if (username?.toLowerCase() === "admin" && password === "pass") {
+    const token = await createToken(username)
+    return res.cookie(COOKIE_KEY, token._id, cookieOptions).send("Ok")
+  }
+
+  res.status(401).send("Not authenticated")
+})
+
+app.get("/api/logout", (req, res) => {
+  console.log("Logging out")
+  const cookies = req.signedCookies[COOKIE_KEY]
+  console.log({ cookies })
+  res.clearCookie(COOKIE_KEY)
+})
+
+app.get("/api/authenticated", authenticateCookie, (req, res) => {
+  console.log("We can see the authenticated data")
+
+  res.send({
+    secret: "something",
+    balance: "$1,000,000,000"
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Cookie Authentication app listening on port: ${port}`)
+})
